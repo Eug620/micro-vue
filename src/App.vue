2ba39af7763507fc120095ba144c528bb0950ed5
eug <yyh3531@163.com>
2022-08-31 15:29:57 +0800

    fix(vue): 版本不同bug
    #5222

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
  * @Author: eug yyh3531@163.com
  * @Date: 2022-07-14 22:24:52
  * @LastEditors: eug yyh3531@163.com
- * @LastEditTime: 2022-08-28 02:49:31
+ * @LastEditTime: 2022-08-31 15:28:52
  * @FilePath: /micro-chat/src/App.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -15,7 +15,6 @@
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import { useUserStore } from '@/store/modules/user'
 import { useSystemStore } from '@/store/modules/app'
-
 // 配置信息
 useSystemStore().reload()
 // 用户信息
