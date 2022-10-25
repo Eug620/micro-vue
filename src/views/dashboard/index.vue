a5aef06887a9cc2899a4c6436863d6f436b58622
eug <yyh3531@163.com>
2022-10-25 15:03:19 +0800

    feat(dashboard): style change

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,20 +1,38 @@
 <!--
- * @Author: eug yyh3531@163.com
- * @Date: 2022-07-14 23:03:40
- * @LastEditors: eug yyh3531@163.com
- * @LastEditTime: 2022-09-18 05:39:31
- * @FilePath: /micro-chat/src/views/dashboard/index.vue
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @Author       : eug yyh3531@163.com
+ * @Date         : 2022-09-16 23:52:04
+ * @LastEditors  : eug yyh3531@163.com
+ * @LastEditTime : 2022-10-25 14:51:53
+ * @FilePath     : /micro-vue/src/views/dashboard/index.vue
+ * @Description  : filename
+ * 
+ * Copyright (c) 2022 by eug yyh3531@163.com, All Rights Reserved. 
 -->
 <template>
-  <mc-container>
-    <h3>dashboard</h3>
-    {{ $t('message.hello') }}
+  <mc-container class="dashboard-container">
+    <div class="dashboard-container-background">
+      <h3>dashboard</h3>
+      {{ $t("message.hello") }}
+    </div>
   </mc-container>
 </template>
 
 <script lang="ts" setup>
 </script>
 
-<style>
+<style lang="scss">
+.dashboard-container {
+  &-background {
+    width: 100%;
+    height: 100%;
+    color: rgba(0, 0, 0, 0);
+    font-size: 200px;
+    font-weight: 900;
+    text-align: center;
+    background: url("https://farm5.staticflickr.com/4273/34378170890_3901a0ac25_k.jpg?momo_cache_bg_uuid=99cbab77-d2d1-4c69-be97-8ba21a5eb59a")
+      no-repeat center/cover;
+    background-clip: text;
+    -webkit-background-clip: text;
+  }
+}
 </style>
